1950c1f20303490199d0c05885ac7295b21f51e0
Jovan11111 <hongjongjo@gmail.com>
2023-10-15 15:24:51 +0200

    Finished Chimp test

--- ChimpTest.py
+++ ChimpTest.py
@@ -28,12 +28,25 @@ TWENTY_TWO = [1142, 1147]
 TWENTY_THREE = [1134, 1145]
 TWENTY_FOUR = [1115, 1153]
 TWENTY_FIVE = [1179, 1184]
-TWENTY_SIX = [1255]
-TWENTY_SEVEN = [984]
+TWENTY_SIX = [1255, 1266]
+TWENTY_SEVEN = [984, 989]
 TWENTY_EIGHT = [1283, 1280]
+TWENTY_NINE = [1254, 1253]
+THIRTY = [1204, 1210]
+THIRTY_ONE = [944, 914]
+THIRTY_TWO = [1146, 1144]
+THIRTY_THREE = []
+THIRTY_FOUR = []
+THIRTY_FIVE = []
+THIRTY_SIX = []
+THIRTY_SEVEN = []
+THIRTY_EIGHT = []
+THIRTY_NINE = []
+FORTY = []
 NUMBERS = [ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, ELEVEN, TWELVE, THIRTEEN, FOURTEEN,
            FIFTEEN, SIXTEEN, SEVENTEEN, EIGHTEEN, NINETEEN, TWENTY, TWENTY_ONE, TWENTY_TWO,
-           TWENTY_THREE, TWENTY_FOUR, TWENTY_FIVE, TWENTY_SIX, TWENTY_SEVEN, TWENTY_EIGHT]
+           TWENTY_THREE, TWENTY_FOUR, TWENTY_FIVE, TWENTY_SIX, TWENTY_SEVEN, TWENTY_EIGHT,
+           TWENTY_NINE, THIRTY, THIRTY_ONE]
 
 
 def chimpTest():
@@ -41,22 +54,30 @@ def chimpTest():
     coordinates = []
     for i in range(5):
         for j in range(8):
-            coordinates.append([j*112, i*112])
-
-    grid = [0] * 5 * 8
-    for x, coordinate in enumerate(coordinates):
-        screenshot = pyautogui.screenshot(region=(coordinate[0]+515, coordinate[1]+275, 80, 80))
-        screenshot_np = np.array(screenshot)
-        grayPic = cv2.cvtColor(screenshot_np, cv2.COLOR_BGR2GRAY)
-        ret, threshold = cv2.threshold(grayPic, 200, 255, cv2.THRESH_BINARY)
-        if cv2.countNonZero(threshold) in grid and cv2.countNonZero(threshold) != 0:
-            grid[x] = cv2.countNonZero(threshold)+1
-        else:
+            coordinates.append([j * 112, i * 112])
+    nums = [ONE, TWO, THREE, FOUR]
+    ind = 4
+    pyautogui.click(950, 730)
+    while True:
+        grid = [0] * 5 * 8
+        for x, coordinate in enumerate(coordinates):
+            screenshot = pyautogui.screenshot(region=(coordinate[0] + 515, coordinate[1] + 275, 80, 80))
+            screenshot_np = np.array(screenshot)
+            grayPic = cv2.cvtColor(screenshot_np, cv2.COLOR_BGR2GRAY)
+            ret, threshold = cv2.threshold(grayPic, 200, 255, cv2.THRESH_BINARY)
             grid[x] = cv2.countNonZero(threshold)
-    print(grid)
+        print(grid)
 
-    for number in NUMBERS:
-        for i, el in enumerate(grid):
-            if el in number:
-                pyautogui.click(coordinates[i][0]+550, coordinates[i][1]+290)
-                break
+        for number in NUMBERS:
+            for i, el in enumerate(grid):
+                if el in number:
+                    if grid[i] == 910 and number == THIRTEEN:
+                        grid[i] -= 1
+                    pyautogui.click(coordinates[i][0] + 550, coordinates[i][1] + 290)
+                    break
+
+        nums.append(NUMBERS[ind])
+        if ind < 30:
+            ind += 1
+        time.sleep(0.5)
+        pyautogui.click(940, 730)
